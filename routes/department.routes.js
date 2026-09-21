0338f1f4771e7966dd0b9b216ca6dfcceace0222
mohamed-ABannai <moabdulla172@gmail.com>
2026-09-21 19:01:09 +0300

    feat: add GET and POST routes for departments

--- routes/department.routes.js
+++ routes/department.routes.js
@@ -1,7 +1,30 @@
 const router = require("express").Router()
-
+const Department=require('../models/Department')
 
 router.get('/',(req,res)=>{
     res.render('homepage.ejs')
 })
+
+
+
+router.get('/create',(req,res)=>{
+
+res.render('./departments/createDepartment.ejs')
+
+})
+
+router.post('/create',async(req,res)=>{
+
+const createDep= await Department.create({
+    name:req.body.name,
+    description:req.body.description
+})
+res.redirect('/departments/create')
+
+})
+
+
+
+
+
 module.exports = router;
