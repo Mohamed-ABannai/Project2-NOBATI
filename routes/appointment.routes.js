3a414d0052d40344dbeb9e8e0d49a7e90730f3ca
mohamed-ABannai <moabdulla172@gmail.com>
2026-09-26 20:28:56 +0300

    feat: add patient appointment booking

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const Appointment = require("../models/Appointment")
 const { isPatient } = require("../middleware/is-signed-in")
+const Doctor = require("../models/Doctor")
+const Department = require("../models/Department")
 
 router.get('/', isPatient, async (req, res) => {
 
@@ -8,7 +10,42 @@ router.get('/', isPatient, async (req, res) => {
         patient: req.session.user._id
     }).populate('doctor')
 
-    res.render('appointments/allAppointments.ejs', { appointments:appointments })
+    res.render('appointments/allAppointments.ejs', { appointments: appointments })
+})
+
+router.get('/new', isPatient, async (req, res) => {
+
+    const generalDepartment = await Department.findOne({
+        name: 'General Medicine'
+    })
+
+    const doctors = await Doctor.find({
+        department: generalDepartment._id,
+        isActive: true
+    }).populate('user')
+
+    res.render('appointments/createAppointments.ejs', { doctors })
+})
+
+
+router.post('/new',isPatient,async(req,res)=>{
+
+    const {
+        doctor,
+        appointmentDate,
+        appointmentTime,
+        reason
+    } = req.body
+
+    const newAppointment = await Appointment.create({
+        patient:req.session.user._id,
+        doctor,
+        appointmentDate,
+        appointmentTime,
+        reason
+    })
+
+    res.redirect('/appointment')
 })
 
 module.exports = router;
