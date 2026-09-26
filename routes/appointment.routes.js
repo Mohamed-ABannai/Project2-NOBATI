55b7dba79b0ad4d4d5e609b2739d4d11c3f7ae03
mohamed-ABannai <moabdulla172@gmail.com>
2026-09-26 20:09:07 +0300

    feat: add patient appointment management page

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -1,5 +1,12 @@
 const router = require("express").Router()
 
+router.get('/', isPatient, async (req, res) => {
 
+    const appointments = await Appointment.find({
+        patient: req.session.user._id
+    }).populate('doctor')
+
+    res.render('appointments/allAppointments.ejs', { appointments })
+})
 
 module.exports = router;
